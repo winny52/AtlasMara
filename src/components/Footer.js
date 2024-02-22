4c40323528279ad92dfc5bea5cd8d01a028143f4
Winny <winny.chelangat2@student.moringaschool.com>
2024-02-22 08:39:55 +0300

    Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
                 </div>
                 <div className="mt-4">
                     <p className="text-center">&copy; Copyright &nbsp;©&nbsp;   2023 Atlas Mara Logistics. All rights reserved.</p>
-                    <p className="text-center mt-2">Our Location:  123 Main St, Kericho, Kenya</p>
+                    <p className="text-center mt-2">Our Location:  2nd Floor,Gateway Mall, Nairobi, Kenya</p>
                 </div>
                 <button onClick={scrollToTop} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Scroll to Top
